abd9ac588db19fe036473b953a7ac35f7d0a9ff8
Marco Francisco <marcoasfran@gmail.com>
2024-11-15 22:42:30 +0000

    documentation init

--- main.go
+++ main.go
@@ -6,29 +6,51 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// getQrCode handles a GET request to generate and return a QR Code for a "Giver"
+// @Summary      Returns QR Code for a Giver
+// @Description  Returns QR Code for a Giver
+// @Tags         qrcode
+// @Produce      json
+// @Success      200   {object}  gin.H  "QR Code for Queue"
+// @Failure      400   {object}  gin.H  "Bad Request"
+// @Router       /qrcode [get]
+func getQrCode(ctx *gin.Context) {
+	ipAddress := ctx.ClientIP()
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"ipAddress": ipAddress,
+	})
+}
+
+// getQrCode handles a GET request retrieve a ticket for a given Queue
+// @Summary      Returns ticket number for the queue
+// @Description  Returns ticket number for the queue
+// @Tags         qrcode, ticket
+// @Produce      json
+// @Success      200   {object}  gin.H  "Ticket number for queue"
+// @Failure      400   {object}  gin.H  "Bad Request"
+// @Router       /ticket [get]
+func getTicket(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"ticket": 1,
+	})
+}
+
+// @title           QR Code Microservice
+// @version         1.0
+// @description     This microservice has the responsability of creating and validating QR Codes for a Queue
+
+// @license.name  MIT
+// @license.url   http://opensource.org/licenses/MIT
+
+// @host      localhost:8080
+// @BasePath  /
 func main() {
 	router := gin.Default()
 
-	// Define routes
-	router.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	router.GET("/qrcode", getQrCode)
 
-	router.GET("/qrcode", func(ctx *gin.Context) {
-		ipAddress := ctx.ClientIP()
-
-		ctx.JSON(http.StatusOK, gin.H{
-			"ipAddress": ipAddress,
-		})
-	})
-
-	router.GET("/ticket", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"ticket": 1,
-		})
-	})
+	router.GET("/ticket", getTicket)
 
 	// Start the server
 	router.Run(":8080") // Listen on port 8080
