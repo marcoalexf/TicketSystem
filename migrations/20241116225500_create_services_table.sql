5985059e3274c18acde69421f370cd3c776064cb
Marco Francisco <marcoasfran@gmail.com>
2024-11-17 08:59:27 +0000

    update script

--- migrations/20241116225500_create_services_table.sql
+++ migrations/20241116225500_create_services_table.sql
@@ -1,10 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE public.services (
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
     email CHARACTER VARYING NOT NULL,
     ipAddress CHARACTER VARYING NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (email, ipAddress)
+    PRIMARY KEY (id, email)
 );
 -- +goose StatementEnd
 
