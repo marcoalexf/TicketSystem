ef9f1b03b018ef6c1f30bcf14a07aae39b0a8798
Marco Francisco <marcoasfran@gmail.com>
2024-11-17 08:55:14 +0000

    fix migrations file

--- migrations/20241116225500_create_services_table.sql
+++ migrations/20241116225500_create_services_table.sql
@@ -1,12 +1,14 @@
--- +goose StatementBegin
 -- +goose Up
-CREATE TABLE services (
+-- +goose StatementBegin
+CREATE TABLE public.services (
     email CHARACTER VARYING NOT NULL,
     ipAddress CHARACTER VARYING NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (email, ipAddress)
 );
+-- +goose StatementEnd
 
 -- +goose Down
+-- +goose StatementBegin
 DROP TABLE services;
 -- +goose StatementEnd
